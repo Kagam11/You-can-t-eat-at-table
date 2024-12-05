0f48aee0ad8c4895f3b7dcc4320fe789a367d3f2
Kagam11 <hiiragi.kagami@foxmail.com>
2024-12-05 11:15:20 +0800

    Initial

---
+++ Source/You can't eat at table/YouCantEatAtTableMod.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace YouCantEatAtTable
+{
+    public class YouCantEatAtTableMod : Mod
+    {
+        public static YouCantEatAtTableModSettings settings;
+
+        public YouCantEatAtTableMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<YouCantEatAtTableModSettings>();
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            settings.DoSettingsWindowContents(inRect);
+        }
+        public override string SettingsCategory()
+        {
+            return "YouCantEatAtTable".Translate();
+        }
+    }
+}
---
+++ Source/You can't eat at table/YouCantEatAtTableModSettings.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace YouCantEatAtTable
+{
+    public class YouCantEatAtTableModSettings : ModSettings
+    {
+        public int CanSlavesAndPrisonersEatAtTable = 2;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref CanSlavesAndPrisonersEatAtTable, "CanSlavesAndPrisonersEatAtTable", 2);
+        }
+        public void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing = new Listing_Standard();
+            listing.Begin(inRect);
+            listing.Label("CanSlavesAndPrisonersEatAtTable".Translate());
+            listing.Label("Explains".Translate());
+            CanSlavesAndPrisonersEatAtTable = (int)listing.Slider(CanSlavesAndPrisonersEatAtTable, 0, 2);
+            listing.End();
+            base.Write();
+        }
+    }
+}
